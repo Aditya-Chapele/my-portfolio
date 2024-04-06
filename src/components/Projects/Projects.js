1936d49b6726f2cba3b57e7676b467aa659cd92c
Aditya Chapele <128784142+Aditya-Chapele@users.noreply.github.com>
2024-04-06 21:54:04 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -61,7 +61,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1> */}
         <h1 style={{ color: "white" }}>
-          Some other <strong className="purple">Front-end </strong>projects which I have worked.
+          Some <strong className="purple">Front-end </strong> projects created by me.
         </h1>
           <Col md={4} className="project-card">
             <ProjectCard
